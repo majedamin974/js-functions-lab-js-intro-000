d5ac622df9bb04767fd68c03d33d78e587277e3a
Majed Amin <majedamin974@gmail.com>
2019-05-28 14:33:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysto(name){
+  return "Happy holidays," + ${name} + "!";
+}
+
+function happyHolidayTo(holiday, name){
+  return "Happy" + ${holiday} + "," + ${name} + "!";
+}
